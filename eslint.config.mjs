a7375d2e4cf88d2a8b2f75723ea0334a712f9b25
shiratama644 <shiratama644@users.noreply.github.com>
2026-06-02 18:39:10 +0900

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-export default [
+const config = [
   {
     ignores: [
       ".next/**",
@@ -53,3 +53,5 @@ export default [
     },
   },
 ];
+
+export default config;
